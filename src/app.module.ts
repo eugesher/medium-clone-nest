82d1a553f0db789b39c4ea1dcfad6793dd4c12e6
Evgenii Chernyshov <mailbox@eugesher.site>
2021-10-05 13:04:22 +0300

    generate 'tags' resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagModule } from './tag/tag.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [TagModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [TagsModule],
 })
 export class AppModule {}
