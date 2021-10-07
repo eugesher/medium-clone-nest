7b4dbb8d186de977c8ec4f6b154629a05dcce2d3
Evgenii Chernyshov <mailbox@eugesher.site>
2021-10-07 13:48:41 +0300

    feat: create, find article

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -3,40 +3,54 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
   Delete,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { AuthGuard } from '../users/guards/auth.guard';
+import { UserDecorator } from '../users/decorators/user.decorator';
+import { User } from '../users/entities/user.entity';
+import { ArticleResponseInterface } from './types/article-response.interface';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
-  create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articlesService.create(createArticleDto);
+  @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
+  async create(
+    @UserDecorator() user: User,
+    @Body('article') dto: CreateArticleDto,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articlesService.create(user, dto);
+    return this.articlesService.buildArticleResponse(article);
   }
 
-  @Get()
-  findAll() {
-    return this.articlesService.findAll();
+  // @Get()
+  // findAll() {
+  //   return this.articlesService.findAll();
+  // }
+
+  @Get(':slug')
+  async findOne(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articlesService.findOne(slug);
+    return this.articlesService.buildArticleResponse(article);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.articlesService.findOne(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+  //   return this.articlesService.update(+id, updateArticleDto);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articlesService.update(+id, updateArticleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.articlesService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.articlesService.remove(+id);
+  // }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { User } from '../users/entities/user.entity';
+import { Article } from './entities/article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ArticleResponseInterface } from './types/article-response.interface';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticlesService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>,
+  ) {}
+
+  private static generateSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
   }
 
-  findAll() {
-    return `This action returns all articles`;
+  buildArticleResponse(article: Article): ArticleResponseInterface {
+    return { article };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async create(user: User, dto: CreateArticleDto): Promise<Article> {
+    const article = new Article();
+    Object.assign(article, dto);
+
+    article.author = user;
+    article.slug = ArticlesService.generateSlug(article.title);
+    if (!article.tagList) article.tagList = [];
+
+    return await this.articleRepository.save(article);
   }
 
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+  // findAll() {
+  //   return `This action returns all articles`;
+  // }
+
+  async findOne(slug: string): Promise<Article> {
+    return await this.articleRepository.findOne(
+      { slug },
+      { relations: ['author'] },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} article`;
-  }
+  // update(id: number, updateArticleDto: UpdateArticleDto) {
+  //   return `This action updates a #${id} article`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} article`;
+  // }
 }
